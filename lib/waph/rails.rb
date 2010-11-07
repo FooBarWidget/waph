01f0b9114f85d1b7d748bd3a902f47b9f17643aa
Hongli Lai (Phusion) <hongli@phusion.nl>
2010-11-07 16:15:51 +0100

    Rails 3 support.

--- lib/waph/rails.rb
+++ lib/waph/rails.rb
@@ -1,8 +1,13 @@
 module Waph
   module Rails
     def self.setup(rails_config, core = Waph::INSTANCE)
-      rails_config.database_configuration_file = core.config_filename(:database)
-      rails_config.log_path = core.log_filename
+      if rails_config.respond_to?(:paths)
+        rails_config.paths.database = core.config_filename(:database)
+        rails_config.paths.log = core.log_filename
+      else
+        rails_config.database_configuration_file = core.config_filename(:database)
+        rails_config.log_path = core.log_filename
+      end
     end
   end
 end
